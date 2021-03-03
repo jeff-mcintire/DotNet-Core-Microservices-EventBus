cbad2bb64071f4f80c2ae2bad8839346dfb63448
Jeff McIntire <me@jeffmcintire.com>
2021-03-02 22:15:17 -0800

    Initial commit of solution and folders

---
+++ SimpleSample/Producer/Sender.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace Producer
+{
+    class Sender
+    {
+        public static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() {HostName = "localhost"};
+            
+            using(var connetion = factory.CreateConnection())
+            using (var channel = connetion.CreateModel())
+            {
+                channel.QueueDeclare("BasicTest", false, false, false, null);
+                string message = "getting started with .net core rabbitmq";
+                var body = Encoding.UTF8.GetBytes(message);
+                
+                channel.BasicPublish("", "BasicTest", null, body);
+                
+                Console.WriteLine("sent messaage {0}...", message);
+            }
+            
+            Console.WriteLine("Press [enter] to exit the Sender App...");
+            Console.ReadLine();
+        }
+    }
+}
